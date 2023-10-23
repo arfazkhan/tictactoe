9e2f1631cf91a0978d3e89fc11a459f6d183d53c
Arfaz Khan <arfuzkhan@gmail.com>
2023-10-23 10:57:38 +0530

    fixed bug

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -41,52 +41,34 @@ const TicTacToe = () => {
         }
     }
     const checkWin = () => {
-        if (data[0]===data[1] && data[1]===data[2] && data[2]!=="")
-        {
+        if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
             won(data[2]);
-        }
-        else if (data[3]===data[4] && data[4]===data[5] && data[5]!=="")
-        {
+        } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
             won(data[5]);
-        }
-        else if (data[6]===data[7] && data[7]===data[8] && data[8]!=="")
-        {
+        } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
             won(data[8]);
-        }
-        else if (data[0]===data[3] && data[3]===data[6] && data[6]!=="")
-        {
+        } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
             won(data[6]);
-        }
-        else if (data[1]===data[4] && data[4]===data[7] && data[7]!=="")
-        {
+        } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
             won(data[7]);
-        }
-        else if (data[2]===data[5] && data[5]===data[8] && data[8]!=="")
-        {
+        } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
             won(data[8]);
-        }
-        else if (data[0]===data[4] && data[4]===data[8] && data[8]!=="")
-        {
+        } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
             won(data[8]);
-        }
-        else if (data[0]===data[1] && data[1]===data[2] && data[2]!=="")
-        {
-            won(data[2]);
-        }
-        else if (data[2]===data[4] && data[4]===data[6] && data[6]!=="")
-        {
+        } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
             won(data[6]);
-        }
-        const isDraw = data.every(cell => cell !== "");
+        } else {
+            const isDraw = data.every(cell => cell !== "");
             if (isDraw) {
-                 setLock(true);
-                 titleRef.current.innerHTML = "It's a Draw!";
+                setLock(true);
+                titleRef.current.innerHTML = "It's a Draw!";
+            }
+        }
     }
-
-    }
+    
     const won = (winner) => {
         setLock(true);
-        if (winner==="x")
+        if (winner==="X")
         {
             titleRef.current.innerHTML= `Congratulations: <img src=${cross_icon}> Wins`
         }
